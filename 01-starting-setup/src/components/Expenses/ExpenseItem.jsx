a82a2a6dc7ede3f59cbfbef91ef9e42b8e1b7c75
Qchown31 <54653694+Qchown31@users.noreply.github.com>
2021-08-11 15:49:07 -0600

    jsx vs js

--- 01-starting-setup/src/components/Expenses/ExpenseItem.jsx
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.jsx
@@ -5,12 +5,16 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked!!");
+  };
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Click Me</button>
       </div>
     </Card>
   );
